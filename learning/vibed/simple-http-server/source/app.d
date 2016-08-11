4b8a8afb8972b23628edb5575a2bf044125f09f9
Andrew Gerst <gerst20051@gmail.com>
2016-08-11 00:05:32 -0400

    use urlrouter

--- learning/vibed/simple-http-server/source/app.d
+++ learning/vibed/simple-http-server/source/app.d
@@ -5,13 +5,22 @@ shared static this()
     auto settings = new HTTPServerSettings;
     settings.port = 8080;
     settings.bindAddresses = ["::1", "127.0.0.1"];
-    listenHTTP(settings, &handleRequest);
+    auto router = new URLRouter()
+        .get("/", &hello)
+        .get("/bye", staticTemplate!"bye.dt")
+        .any("*", &handleRequest);
+    listenHTTP(settings, router);
 }
 
 void handleRequest(HTTPServerRequest req, HTTPServerResponse res)
 {
-    if (req.path == "/")
+    if (req.path == "/index")
         res.render!"index.dt";
     else
         res.writeBody("Hello, World!", "text/plain");
+}
+
+void hello(HTTPServerRequest req, HTTPServerResponse res)
+{
+    res.render!"index.dt";
 }
